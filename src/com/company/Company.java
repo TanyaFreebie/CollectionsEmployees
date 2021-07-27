55219e20dd497926ea28c31abe76761bce67a780
TanyaFreebie <escurio@gmail.com>
2021-07-27 18:32:53 +0300

    Stuck on main class adding new lists in company.arraylist

---
+++ src/com/company/Company.java
@@ -0,0 +1,42 @@
+package com.company;
+
+import java.util.List;
+
+
+public class Company {
+
+    private List<Employee> employeeList;
+    private String companyName;
+
+    //add an constructor
+    public Company(String companyName, List<Employee> employeeList){
+        this.companyName = companyName;
+        this.employeeList = employeeList;
+    }
+
+    public float getAverage(){
+        float sum = 0;
+        for (int i = 0; i < this.employeeList.size(); i++) {
+            sum += this.employeeList.get(i).getAge();
+        }
+        return sum / this.employeeList.size();
+    }
+
+    public List<Employee> getEmployeeList() {
+        return employeeList;
+    }
+
+    public void setEmployeeList(List<Employee> employeeList) {
+        this.employeeList = employeeList;
+    }
+
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //2nd Task
+//         Employee[] javaEmployees = {
+//                new Employee("George", 35, "J12", "Microsoft"),
+//                new Employee("Dana", 29, "H15", "Microsoft"),
+//                new Employee("Harry", 28, "U23", "Microsoft")
+//        };
+//
+//        Employee[] javascriptEmployees = {
+//                new Employee("Nathan", 31, "I91", "Google"),
+//                new Employee("Jane", 42, "A22", "Google"),
+//                new Employee("Jordan", 22, "O23", "Google"),
+//        };
+//        Company microsoft =new Company("Microsoft", javaEmployees);
+//        Company google =new Company("Google", javascriptEmployees);
+////        System.out.println("The average age of employees in Microsoft is " + microsoft.getAverage());
+////        System.out.println("The average age of employees in Google is " + google.getAverage());
+//        System.out.println(google.getEmployee().getName());
+
+        List <Employee> javaEmployees = ????? {
+        }
+
+    }
+
+
+}
