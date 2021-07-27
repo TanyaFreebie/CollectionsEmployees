fde7056dbfb1af858a5b10d3166a4a2935c989c2
TanyaFreebie <escurio@gmail.com>
2021-07-27 19:24:01 +0300

    Now I'm stuck here

--- src/com/company/Company.java
+++ src/com/company/Company.java
@@ -1,12 +1,14 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
-public class Company {
 
-    private List<Employee> employeeList;
+public class Company {
     private String companyName;
+    private List<Employee> employeeList;
+
 
     //add an constructor
     public Company(String companyName, List<Employee> employeeList){
